028384885c810cd67b98c649e7222fb14eb0c3d3
Heather Frantisak <hfrantisak@mvnp.com>
2015-08-12 10:21:01 -1000

    Initial commit

---
+++ js/controllers.js
@@ -0,0 +1,12 @@
+var jsonApp = angular.module('jsonApp', []);
+
+jsonApp.controller('jsonDataCtrl', ['$scope', '$http', function($scope, $http) {
+  // SF open data, public art
+  $scope.jsonData = $http.get('https://data.sfgov.org/resource/zfw6-95su.json')
+                  .success(function(data) {
+                    $scope.jsonData = data;
+                  })
+                  .error(function(err) {
+                    console.error(err);
+                  });
+}]);
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Parsing JSON with Angular
+
+Proof of concept for getting and parsing JSON data using Angularjs.
+
+TODO -- Visualize large datasets with d3js.
